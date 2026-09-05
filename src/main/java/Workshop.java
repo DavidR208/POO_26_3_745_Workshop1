b6734aefc9472c60347447b0d4cf042d5d54a59a
DavidR208 <androdriguezc@udistrital.edu.co>
2026-09-04 21:19:52 -0500

    Para este ejercicio primero se evita que este vacio o nulo, pero para hallar el mayor primero se toma por un momento que el numero ubicado en cero es el mayor hasta que el programa hacer un bucle por el arreglo y determina el mayor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -137,6 +137,16 @@ public class Workshop {
         // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
         return 0;
+	if (arreglo == null || arreglo.length == 0) {
+        	throw new IllegalArgumentException("El arreglo no puede ser nulo o estar vacío.");
+    	}
+	int mayor = arreglo[0];
+	for (int i = 1; i < arreglo.length; i++) {
+        	if (arreglo[i] > mayor) {
+            		mayor = arreglo[i]; // Se registra nuev numero mayor
+        	}
+    	}
+	return mayor;
     }
 
     // Método que encuentra el elemento menor en un arreglo
