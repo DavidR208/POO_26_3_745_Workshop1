af95a89629e744e6ba432f2d407f7cfbe072b6b3
DavidR208 <androdriguezc@udistrital.edu.co>
2026-09-09 10:19:13 -0500

    En este se busca se sacar el promedio de una lista de numeros donde aca se usa un bucle que suma todos los numeros de la lista, determiando si es nula como 0, y para el resultado se toma la suma de los elementos y se divide por el tamaño de la lista con lista.size()

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -472,11 +472,22 @@ public class Workshop {
     }
 
     // Método que calcula el promedio de una lista de números
-
     public double promedioLista(List<Integer> lista) {
         // TODO: Implementar el método para calcular el promedio de una lista de números.
         // Ejemplo: Si lista = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
-        return 0.0;
+	if (lista == null || lista.isEmpty()) {
+        	return 0.0;
+    	}
+	
+	double suma = 0;
+
+	// Suma de los numeros de la lista
+	for (Integer numero : lista) {
+        	if (numero != null) { // Protección en caso de que la lista contenga nulos
+            		suma += numero;
+        	}
+    	}
+        return suma / lista.size();
     }
 
     // Método que convierte un número en su representación binaria
