1d75f58393a4d8d027e099959e55b2b5ece8fb07
DavidR208 <androdriguezc@udistrital.edu.co>
2026-09-13 14:20:04 -0500

    En este ultimo trabajo colocamos en el rango de los meses y los dias, despues los dias de 30 dias y por ultimo el mes de febrero a 29 dias, por si es bisiesto cuando nacio, despues con las fechas de cada signo zodiacal se establecio el rango de cada uno para que cuando coincidan estas fechas retorne el signo correspondiente y si ingresa algo que no es se retorna fecha invalida

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -597,7 +597,45 @@ public class Workshop {
     }
 
     public String zoodiac(int day, int month) {
-        return "";
+	// Validar meses y rango de dias
+    	if (month < 1 || month > 12 || day < 1 || day > 31) {
+        	return "Fecha inválida";
+    	}
+    	// Meses con 30 dias
+   	if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+        	return "Fecha inválida";
+    	}
+	// Fechas de febrero
+    	if (month == 2 && day > 29) { 
+        	return "Fecha inválida";
+    	}
+	// Signos
+	if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
+        	return "Capricornio";
+    	} else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+        	return "Acuario";
+    	} else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        	return "Piscis";
+    	} else if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+        	return "Aries";
+    	} else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+        	return "Tauro";
+    	} else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        	return "Géminis";
+    	} else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+        	return "Cáncer";
+    	} else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+        	return "Leo";
+    	} else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+        	return "Virgo";
+    	} else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+        	return "Libra";
+    	} else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+        	return "Escorpio";
+    	} else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+        	return "Sagitario";
+    	}
+        return "Fecha invalida";
     }
 
 
